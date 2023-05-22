b22bd6f04baf326e640e80516eeed351f6404b49
toka <t.abdelhamied@egyptsmartcards.com>
2023-05-22 16:11:55 +0300

    pagination, limit, sort, select

--- controllers/tour.js
+++ controllers/tour.js
@@ -91,24 +91,54 @@ exports.getTour = async (req, res) => {
     });
   }
 };
-
+exports.aliasTopTours = async (req, res, next) => {
+  try {
+    req.query.limit = 5;
+    req.query.sort = "-ratingsAverage,price ";
+    req.query.fields = "name, ratingsAverage, price, difficulty,summary ";
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "failure",
+      message: "Invalide request",
+    });
+  }
+  next();
+};
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
     let queryObj = { ...req.query };
-    let excludedFeilds = ["limit", "sort", "page", "field"];
+    let excludedFeilds = ["limit", "sort", "page", "fields"];
     excludedFeilds.forEach((e) => delete queryObj[e]);
     console.log("queryObj", queryObj);
     let query = Tour.find(queryObj);
-
+    // sorting
     if (req.query.sort) {
       const sortedBy = req.query.sort.split(",").join(" ");
       query = query.sort(sortedBy);
     } else {
       query = query.sort("-createdAt");
     }
+    // spicify fields
+    if (req.query.fields) {
+      const selectedBy = req.query.fields.split(",").join(" ");
+      query = query.select(selectedBy);
+    } else {
+      query = query.select("");
+    }
+    // pagination
+    const limit = req.query.limit * 1 || 100;
+    const page = req.query.page * 1 || 1;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const count = await Tour.countDocuments();
+      if (skip >= count) {
+        throw new Error("this page dosn't exist");
+      }
+    }
     const tours = await query;
-
     res.status(200).json({
       status: "success",
       data: {
--- routes/tour.js
+++ routes/tour.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const toursController = require("../controllers/tour");
+
 router
   .route("/")
   .get(toursController.getAllTours)
   .post(toursController.createTour);
+router
+  .route("/top-five-tours")
+  .get(toursController.aliasTopTours, toursController.getAllTours);
 router
   .route("/:id")
   .get(toursController.getTour)
